f3709b1bfc561545a070f39b4b885aea744222b7
ramaj <rama.jagadesh@gmail.com>
2026-04-30 16:12:52 +0800

    chore: fixed auth validations

--- backend/validators/authValidator.js
+++ backend/validators/authValidator.js
@@ -1,18 +1,31 @@
 const Joi = require("joi");
 
+const passwordSchema = Joi.string()
+  .min(8)
+  .pattern(/[a-z]/, "lowercase letter")
+  .pattern(/[A-Z]/, "uppercase letter")
+  .pattern(/[0-9]/, "number")
+  .pattern(/[^a-zA-Z0-9]/, "special character")
+  .required()
+  .messages({
+    "string.min": "Password must be at least 8 characters long",
+    "string.pattern.name":
+      "Password must include at least one {#name}",
+    "string.empty": "Password is required"
+  });
+
 const signupSchema = Joi.object({
   name: Joi.string().min(2).required(),
 
   email: Joi.string().email().required(),
 
-  password: Joi.string().min(6).required(),
+  password: passwordSchema,
 
   role: Joi.string().valid("student", "teacher").required()
 });
 
 const loginSchema = Joi.object({
   email: Joi.string().email().required(),
-
   password: Joi.string().required()
 });
 
